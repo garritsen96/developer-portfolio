287a3170626a5ae812b69986632852648f6cd093
Tuna Hacıoğlu <tunahaci04@gmail.com>
2026-05-21 22:04:48 +0300

    First Version of my Portfolio

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,9 @@ import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Abu Said - Software Developer",
+  title: "Portfolio of Tuna Hacıoğlu - Java Developer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "Portfolio of Tuna Hacıoğlu. Java Developer specializing in Spring Boot, backend architectures, and scalable web applications.",
 };
 
 export default function RootLayout({ children }) {
